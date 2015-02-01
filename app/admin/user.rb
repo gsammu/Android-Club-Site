f6a15bcc9953861f38742c9667ea7ab656080167
Anonoz Chong <honsiongchs@gmail.com>
2015-02-02 00:02:24 +0800

    Fixed the bug that username field isnt inside the form

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -3,7 +3,10 @@ ActiveAdmin.register User do
   permit_params :username, :email, :password, :password_confirmation
 
   form do |f|
+    f.semantic_errors *f.object.errors.keys
+    
     f.inputs do
+      f.input :username
       f.input :email
       f.input :password
       f.input :password_confirmation
