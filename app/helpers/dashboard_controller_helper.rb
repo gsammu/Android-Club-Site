f65f371dcb47f36039df4d7cdeb39bc9b37628d2
Anonoz Chong <honsiongchs@gmail.com>
2015-02-28 13:21:52 +0800

    Added basic layout of dashboard checklist

--- app/helpers/dashboard_controller_helper.rb
+++ app/helpers/dashboard_controller_helper.rb
@@ -1,2 +1,10 @@
 module DashboardControllerHelper
+  def humanize_countdown(deadline)
+    if Time.now > deadline
+      "Expired"
+    else
+      distance_of_time_in_words(Time.now, deadline).capitalize +
+      " left. "
+    end
+  end
 end
