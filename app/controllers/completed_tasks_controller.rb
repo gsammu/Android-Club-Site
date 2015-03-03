82756048077ca2b065191d696efc4173773c1f33
HiiYL <yonglian146@gmail.com>
2015-03-03 12:22:49 +0800

    refactored AJAX to use POST and DELETE

--- app/controllers/completed_tasks_controller.rb
+++ app/controllers/completed_tasks_controller.rb
@@ -5,11 +5,7 @@ class CompletedTasksController < ApplicationController
   end
 
   def create
-    if (params[:is_checked].to_s == "true")
-      current_user.completed_tasks.build(completed_task_params)
-    else
-      current_user.completed_tasks.where(completed_task_params).destroy_all
-    end
+    current_user.completed_tasks.build(completed_task_params)
     if current_user.save
       respond_to do |format|
         format.html do
@@ -30,7 +26,28 @@ class CompletedTasksController < ApplicationController
       end
     end
   end
-
+  def destroy
+    current_user.completed_tasks.where(completed_task_params).destroy_all
+    if current_user.save
+      respond_to do |format|
+        format.html do
+          redirect_to root_url, notice: "Save process completed!"
+        end
+        format.json do
+          render json: nil, status: :ok
+        end
+      end
+    else
+      respond_to do |format|
+        format.html do
+          redirect_to dashboard_url, notice: "Save failed"
+        end
+        format.json do
+          render json: { success: false } 
+        end
+      end
+    end
+  end
   def completed_task_params
     params.require(:completed_task).permit(:id, :todo_item_id, :is_checked)
   end
