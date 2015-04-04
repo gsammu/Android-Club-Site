24a45555aa528960ee39b38c2ceaab3be8862bef
Anonoz Chong <honsiongchs@gmail.com>
2015-04-04 23:26:16 +0800

    Fixing styling and homepage copy

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,10 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( home.js dashboard.js sessions.js )
+Rails.application.config.assets.precompile += [
+  "home.js",
+  "dashboard.js",
+  "sessions.js",
+  "home.css",
+  "dashboard.css"
+]
