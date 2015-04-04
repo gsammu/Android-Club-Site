3f79d73998ccd6b9a3575842ba9b851652056eb6
Anonoz Chong <honsiongchs@gmail.com>
2015-04-04 23:10:38 +0800

    Fixed todo list ordering logic and make it stop showing events from too long time ago

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,10 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @todo_lists = TodoList.includes(:todo_items).all.order(deadline: :asc)
+    @todo_lists = TodoList.includes(:todo_items).
+                           where("deadline > ?", Time.now - 2.days).
+                           order(deadline: :asc)
+
     @completed_task = CompletedTask.new
   end
 end
